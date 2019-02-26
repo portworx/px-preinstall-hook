f0addd80db756349440070eae530551fef74e8e9
Harsh Desai <harsh@portworx.com>
2019-02-26 11:04:28 -0800

    Fix syntax in etcd status check script
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- etcdStatus.sh
+++ etcdStatus.sh
@@ -13,20 +13,21 @@ then
     echo "Incorrect ETCD URL provided. It is either not reachable, is incorrect or does not have the tls certificates placed in the right directory(if this is the case please validate the manner to provide certs to Portworx on docs.portworx.com)..."
     echo "Provided etcd url is not reachable or is incorrect. Exiting.." > /dev/termination-log
     exit 1
-fi 
+fi
 }
 
 IFS=';' read -ra array <<< "$svcname"
 for url in "${array[@]}"
-do 
+do
 etcdURL=$(echo "$url" | awk -F: '{ st = index($0,":");print substr($0,st+1)}')
 
 echo "Verifying if the provided etcd url is accessible: $etcdURL"
 
-#Verify with certs if it is a secured etcd. 
+#Verify with certs if it is a secured etcd.
 if [[ ! -z $etcdca ]]
 then
     if [[ ! -z $etcdcert ]]
+    then
         echo "Verifying connectivity to secure etcd... The certs need to be at the location $etcdca $etcdcert and $etcdkey"
         response=$(curl --write-out %{http_code} --silent --output /dev/null --cacert $etcdca --cert $etcdcert --key $etcdkey -L "$etcdURL/version" )
         echo "Response Code: $response"
